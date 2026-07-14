bebadbb77ce1f9a2291eefeac5da19974a1387ed
obrekos6 <obrekos6@gmail.com>
2026-07-14 11:10:02 +0300

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,9 @@
 import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/part.ai/', // ← название твоего репозитория
+});
 
 export default defineConfig({
   server: {
