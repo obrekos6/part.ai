d2f46f6c32dc9e8a01919c45cdad431ea2d78c57
obrekos6 <obrekos6@gmail.com>
2026-07-14 11:16:49 +0300

    Fix vite.config.js - merge configs

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
-import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/part.ai/', // ← название твоего репозитория
-});
-
-export default defineConfig({
+  base: '/part.ai/', // ← для GitHub Pages
   server: {
     host: '127.0.0.1',
     port: 5173,
