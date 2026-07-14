636b8ce4ff624ad6b3dd23a9ade6ea98d8a12985
obrekos6 <obrekos6@gmail.com>
2026-07-14 11:18:18 +0300

    Simplify vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/part.ai/', // ← для GitHub Pages
-  server: {
-    host: '127.0.0.1',
-    port: 5173,
-  },
+  base: '/part.ai/',
 });
